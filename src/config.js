591fd84c1030e70d17c1f69767b97939ef9234bc
Shishak <semenshishak@gmail.com>
2019-08-27 16:38:51 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export const config = {
     title: `React WordPress REST API Demo`,
     description: `Create a single page web app with ReactJS using WordPress as a headless CMS.`,
     author: `@gregrickaby`,
-    siteUrl: `https://nifty-goldstine-53c79a.netlify.com/`
+    siteUrl: `https://lviv.netlify.com/`
   },
   options: {
     wordPressUrl: "https://texty.xyz/wp-json/wp/v2/posts"
